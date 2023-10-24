ccc1c975febf671e491f5f496e5c72c5bb010c4c
Changmin Lee <111254126+DiosAmor@users.noreply.github.com>
2023-10-24 11:03:33 +0900

    v4
    완

--- ToDo-list/script.js
+++ ToDo-list/script.js
@@ -1,6 +1,7 @@
 const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
 
+const savedWeatherData = JSON.parse(localStorage.getItem("saved-weather"));
 const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
 
 const createTodo = function (storageData) {
@@ -37,7 +38,7 @@ const createTodo = function (storageData) {
 };
 
 const keyCodeCheck = function () {
-  if (window.event.keyCode === 13 && todoInput.value !== "") {
+  if (window.event.keyCode === 13 && todoInput.value.trim() !== "") {
     createTodo();
   }
 };
@@ -69,3 +70,89 @@ if (savedTodoList) {
     createTodo(i);
   }
 }
+
+const weatherDataActive = function ({ location, weather }) {
+  const weatherMainList = [
+    "Clear",
+    "Clouds",
+    "Drizzle",
+    "Fog",
+    "Rain",
+    "Snow",
+    "Thunderstorm",
+  ];
+  weather = weatherMainList.includes(weather) ? weather : "Fog";
+  const locationNameTag = document.querySelector("#location-name-tag");
+  locationNameTag.textContent = location;
+  document.body.style.backgroundImage = `url('./images/${weather}.jpg')`;
+
+  if (
+    !savedWeatherData ||
+    savedWeatherData.location !== location ||
+    savedWeatherData.weather !== weather
+  ) {
+    localStorage.setItem(
+      "saved-weather",
+      JSON.stringify({ location, weather })
+    );
+  }
+};
+
+const weatherSearch = function ({ latitude, longitude }) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${APIKey}`
+  )
+    .then((res) => {
+      return res.json();
+    })
+    .then((json) => {
+      console.log(json.name, json.weather[0].main);
+      const weatherData = {
+        location: json.name,
+        weather: json.weather[0].main,
+      };
+      weatherDataActive(weatherData);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const accessToGeo = function ({ coords }) {
+  const { latitude, longitude } = coords;
+  // shorthand property
+  const positionObj = {
+    latitude,
+    longitude,
+  };
+  weatherSearch(positionObj);
+};
+
+const askForLocation = function () {
+  navigator.geolocation.getCurrentPosition(accessToGeo, (err) => {
+    console.log(err);
+  });
+};
+askForLocation();
+if (savedWeatherData) {
+  weatherDataActive(savedWeatherData);
+}
+
+// promise: fulfilled, pending, and rejected
+// const promiseTest = function () {
+//   return new Promise((resolver, reject) => {
+//     setTimeout(() => {
+//       resolver(100);
+//     }, 2000);
+//   });
+// };
+
+// promiseTest().then((res) => {
+//   console.log(res);
+// });
+
+// spread 연산자를 이용한 얕은 복사
+// const copyObj = {...Obj}
+
+// josn stringify를 이용한 깊은 복사
+// const copyObj = JSON.parse(JSON.stringify(Obj))
