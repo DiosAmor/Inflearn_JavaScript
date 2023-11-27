7a56c600f9df9260cc14be56f9993b0c29d6c00a
Changmin Lee <111254126+DiosAmor@users.noreply.github.com>
2023-11-27 16:24:37 +0900

    frontend-v1

---
+++ frontend/lecture-react/1-vanilla/js/Controller.js
@@ -0,0 +1,24 @@
+const tag = "[Controller]";
+
+export default class Controller {
+  constructor(store, {searchFormView}) {
+    console.log(tag);
+    this.store = store;
+
+    this.searchFormView = searchFormView;
+
+    this.subscribeViewEvents();
+  }
+
+  subscribeViewEvents() {
+    this.searchFormView.on("@submit", event => this.search(event.detail.value)).on("@reset", () => this.reset());
+  }
+
+  search(searchKeyword) {
+    console.log(tag, "search", searchKeyword);
+  }
+
+  reset() {
+    console.log(tag, "reset");
+  }
+}
