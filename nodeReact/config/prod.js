47c5363864e501718628382005ec57a50e9d6987
Changmin Lee <111254126+DiosAmor@users.noreply.github.com>
2024-02-07 18:26:17 +0900

    express-v0

---
+++ nodeReact/config/prod.js
@@ -0,0 +1,3 @@
+module.export = {
+  mongoURI: process.env.test,
+};
---
+++ nodeReact/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded 해석
+app.use(bodyParser.urlencoded({ extended: true }));
+// application/json 해석
+app.use(bodyParser.json());
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/register", async (req, res) => {
+  const user = new User(req.body);
+
+  try {
+    await user.save();
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    return res.json({ success: false, err });
+  }
+});
+
+app.post("/login", (req, res) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "해당하는 유저가 없음.",
+      });
+    }
+
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+    });
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
