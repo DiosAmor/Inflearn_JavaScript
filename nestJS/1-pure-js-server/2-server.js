e332ed7db91e4ebc89507afc1ff9fb2c7928a249
Changmin Lee <111254126+DiosAmor@users.noreply.github.com>
2024-02-26 19:35:36 +0900

    NestJS-1

---
+++ nestJS/1-pure-js-server/2-server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+const host = "localhost";
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Home Page</h1>");
+});
+
+app.get("/post", (req, res) => {
+  res.send("<h1>Post Page</h1>");
+});
+
+app.get("/user", (req, res) => {
+  res.send("<h1>User Page</h1>");
+});
+
+app.use((req, res) => {
+  res.status(404).send("<h1>404 Page Not Found!</h1>");
+});
+
+app.listen(3000, () => {
+  console.log(`server running on http://${host}:${port}`);
+});
